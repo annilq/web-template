e4aca71c53823069cc025d05b33b68fcbf6956e9
annilq <410493517@qq.com>
2018-07-25 14:05:57 +0800

    build功能优化

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,49 @@
 // 添加引用
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var ejs = require("gulp-ejs")
+var browserSync = require("browser-sync").create();
 var reload = browserSync.reload;
-var nodemon = require('gulp-nodemon');
+var nodemon = require("gulp-nodemon");
 
 //这个可以让express启动
 gulp.task("node", function() {
-    nodemon({
-        script: './bin/www',
-        ext: 'js ejs',
-        env: {
-            'NODE_ENV': 'development'
-        }
-    })
+  nodemon({
+    script: "./bin/www",
+    ext: "js ejs",
+    env: {
+      NODE_ENV: "development"
+    }
+  });
+});
+
+//生成静态页面
+gulp.task("build", function() {
+  gulp
+    .src("./views/*.ejs")
+    .pipe(
+      ejs({
+        msg: "Hello Gulp!"
+      },{}, { ext: '.html' })
+    )
+    .pipe(gulp.dest("./public"));
 });
 
 // gulp.task('js-watch', ['js'], browserSync.reload);
 
-gulp.task('server',["node"], function() {
+gulp.task("server", ["node"], function() {
+  var files = ["views/**/*.ejs", "routes/**/*.js", "public/**/*.css", "app.js"];
 
-    var files = [
-        'views/**/*.ejs',
-        'routes/**/*.js',
-        'public/**/*.css',
-        'app.js'
-    ];
+  browserSync.init(files, {
+    proxy: "http://localhost:3000",
+    browser: ["chrome", "safari"],
+    notify: false,
+    port: 4001,
+    open: false,
+    ghostMode: {
+      clicks: true,
+      scroll: true
+    }
+  });
 
-    browserSync.init(files, {
-        proxy: 'http://localhost:3000',
-        browser: ['chrome','safari'],
-        notify: false,
-        port: 4001,
-        open:false,
-        ghostMode: {
-            clicks: true,
-            scroll: true
-        }
-    });
-
-    gulp.watch(files).on("change", reload);
+  gulp.watch(files).on("change", reload);
 });
