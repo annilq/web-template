c3b5a4bed51a30415e79cb9af516494c99e3511f
annilq <410493517@qq.com>
2018-08-20 16:32:59 +0800

    样式修改

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -18,10 +18,62 @@ $("a").click(function(e) {
 window.slideout = new Slideout({
   panel: document.getElementById("panel"),
   menu: document.getElementById("menu"),
-  padding:90,
-  touch:false,
+  padding: 90,
+  touch: false,
   tolerance: 70
 });
 $(".mobile-menu-icon").click(function(e) {
   window.slideout.toggle();
 });
+// 顶部menu交互
+$(".menu .menu-item").hover(
+  function(e) {
+    clearTimeout(window.timer);
+    var $tar = $(e.currentTarget),
+      $submenu = $(".submenu");
+    $(".sub-menu-container").show();
+    var offset = $tar.offset();
+    var tarWidth = $tar.width();
+    var menuWidth = $submenu.width();
+    var menuOffset = offset.left - menuWidth / 2 + tarWidth / 2;
+    $submenu.css("left", menuOffset);
+  },
+  function(e) {
+    window.timer = setTimeout(function() {
+      $(".sub-menu-container").hide();
+    }, 200);
+  }
+);
+
+// 底部footer事件
+$(".sub-menu-container").hover(
+  function(e) {
+    clearTimeout(window.timer);
+  },
+  function(e) {
+    $(e.currentTarget).hide();
+  }
+); // 底部footer事件
+$(".link-items .link-item,[data-submenu]").hover(
+  function(e) {
+    clearTimeout(window.timer);
+    var $target = $(e.currentTarget);
+    var menuId = $target.data("menuid");
+    $("[data-submenu=" + menuId + "]").show();
+  },
+  function(e) {
+    clearTimeout(window.timer);
+    window.timer = setTimeout(function() {
+      $("[data-submenu]").hide();
+    }, 200);
+  }
+);
+// 底部footer事件
+$("[data-submenu]").hover(
+  function(e) {
+    clearTimeout(window.timer);
+  },
+  function(e) {
+    $(e.currentTarget).hide();
+  }
+);
