b6f037347d64a8654e87a8088f86e34223d7a80b
annilq <410493517@qq.com>
2018-09-19 17:56:49 +0800

    新闻样式修改

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,11 +1,17 @@
 $(".back-up").click(function() {
-  $("body,html").animate(
-    {
-      scrollTop: 0
-    },
-    300
-  );
+	scrollToTop();
 });
+
+function scrollToTop(){
+	if(navigator.userAgent.indexOf("Mobile") < 0){
+		  $("body,html").animate(
+				    {
+				      scrollTop: 0
+				    },
+				    300
+				  );
+	}
+}
 if (navigator.userAgent.indexOf("Mobile") >-1) {
   window.slideout = new Slideout({
     panel: document.getElementById("panel"),
@@ -27,19 +33,23 @@ if (navigator.userAgent.indexOf("Mobile") >-1) {
 // 顶部menu交互
 $(".menu .menu-item").hover(
   function(e) {
-    clearTimeout(window.timer);
-    var $tar = $(e.currentTarget),
-      $submenu = $(".submenu");
-    $(".sub-menu-container").show();
-    var menuId = $tar[0].id;
-    $submenu.hide();
-    var $curSubmenu=$("#sub"+menuId);
-    var offset = $tar.offset();
-    var tarWidth = $tar.width();
-    var menuWidth = $curSubmenu.width();
-    var menuOffset = offset.left - menuWidth / 2 + tarWidth / 2;
-    $curSubmenu.show();
-    $curSubmenu.css("left", menuOffset);
+	    clearTimeout(window.timer);
+	    var $tar = $(e.currentTarget);
+	    if($("#sub"+$tar.attr('id')).length > 0){
+	        var $submenu = $(".submenu");
+	        $(".sub-menu-container").show();
+	        var menuId = $tar[0].id;
+	        $submenu.hide();
+	        var $curSubmenu=$("#sub"+menuId);
+	        var offset = $tar.offset();
+	        var tarWidth = $tar.width();
+	        var menuWidth = $curSubmenu.width();
+	        var menuOffset = offset.left - menuWidth / 2 + tarWidth / 2;
+	        $curSubmenu.show();
+	        $curSubmenu.css("left", menuOffset);
+	    }else{
+	        $(".sub-menu-container").hide();
+	    }
   },
   function(e) {
     window.timer = setTimeout(function() {
